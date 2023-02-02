5b1120d5b18d766f2892020eddad960c34054f88
MicahBear <micah.b.elm@gmail.com>
2023-02-01 19:34:44 -0800

    import css file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,22 @@
 function App() {
   return (
     <div className="App">
-      <main>
-        <section>
+      <main className="flex">
+        <section className="">
           <div>
-            <h1>Personal Hoe</h1>
+            <h1 className="bg-slate-500">Personal Hoe</h1>
             <h1>Your Gardening Assistant</h1>
             <p>
               Personal Hoe creates an easy follow grow calendar based on your
               grow zone.
             </p>
-            <p> Provides a daily forecast and aggregates your plant notes.</p>
+            <span>
+              Provides a daily forecast and aggregates your plant notes.
+            </span>
           </div>
           <div>
             <form>
+              <span>Sign up to get access to beta.</span>
               <input
                 type="text"
                 placeholder="youremail@email.com"
@@ -23,8 +26,12 @@ function App() {
             </form>
           </div>
         </section>
-        <section>
-          <img src="" alt="image of website mockup" srcset="" />
+        <section className="">
+          <img
+            src="./src/imgs/screely-1675305057167.png"
+            alt="image of website mockup"
+            srcset=""
+          />
         </section>
       </main>
     </div>
